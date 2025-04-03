4fd86e6a2f35b50ad07aff01b1c6567bcf1d1a85
nicolas-montealegre <nmontealegre@contractor.renuityhome.com>
2025-04-03 15:39:10 -0500

    Fixed: Trait HasExternal

--- Traits/HasExternal.php
+++ Traits/HasExternal.php
@@ -7,17 +7,28 @@ class HasExternal
   public function getInstances()
   {
     return [
-      'relations' => ['external']
+      'relations' => ['external', 'externals']
     ];
   }
 
   /**
-   * Relation External
+   * Get the single external relation
    *
    * @param $model
    * @return mixed
    */
   public function external($model)
+  {
+    return $model->morphOne('Modules\Iexternal\Entities\External', 'entity');
+  }
+
+  /**
+   * Get multiple external relations
+   *
+   * @param $model
+   * @return mixed
+   */
+  public function externals($model)
   {
     return $model->morphMany('Modules\Iexternal\Entities\External', 'entity');
   }
